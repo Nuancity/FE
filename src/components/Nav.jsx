3c4ea03a52ff558a7f92433563fa003502c8149e
Yanna <yannafaith97@gmail.com>
2020-12-12 23:24:12 -0700

    disables functionality for editing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,56 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
-import { green } from '@material-ui/core/colors';
-import { Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
-import { yellow } from '@material-ui/core/colors';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { green } from "@material-ui/core/colors";
+import { Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import { yellow } from "@material-ui/core/colors";
+import { Link } from "react-router-dom";
 
 const Nav = styled.nav`
-    top: 0;
-    z-index: 1;
-    height: 10vh;
-    display: flex;
-    position: sticky;
-    align-items: center;
-    background-color: #393a4d;
-    box-shadow: 0px 0px 20px -7px rgba(0, 0, 0, 0.5);
-    justify-content: space-between;
-`
-    const NavLinks = styled.div`
-        display: flex;
-        justify-content: space-around;
-        margin-right: 20px;
-        width: 40vw;
-    `
+  top: 0;
+  z-index: 1;
+  height: 10vh;
+  display: flex;
+  position: sticky;
+  align-items: center;
+  background-color: #393a4d;
+  box-shadow: 0px 0px 20px -7px rgba(0, 0, 0, 0.5);
+  justify-content: space-between;
+`;
+const NavLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-right: 20px;
+  width: 40vw;
+`;
 
 const styles = () => ({
-    upgradeButton: { 
-        borderColor: '#c77d1f',
-        color: "#c77d1f", 
-        '&:hover': {
-            backgroundColor: yellow[ 50 ],
-            borderColor: "#f5a122", 
-        },
+  upgradeButton: {
+    borderColor: "#c77d1f",
+    color: "#c77d1f",
+    "&:hover": {
+      backgroundColor: yellow[50],
+      borderColor: "#f5a122",
     },
-    textLink: {
-        color: 'whitesmoke',
+  },
+  textLink: {
+    color: "whitesmoke",
+  },
+  browseButton: {
+    borderColor: "#009588",
+    color: "#009588",
+    "&:hover": {
+      backgroundColor: green[50],
+      borderColor: "#009588",
     },
-    browseButton: { 
-        borderColor: "#009588", 
-        color: "#009588", 
-        '&:hover': {
-            backgroundColor: green[ 50 ],
-            borderColor: "#009588", 
-        },
-    },
-})
+  },
+});
 
-const LandingNav = ( props ) => {
-    const { session, classes } = props;
-    return (
-        <Nav>
-            <NavLinks >
+const LandingNav = (props) => {
+  const { session, classes } = props;
+  return (
+    <Nav>
+      {/* <NavLinks >
                 <Link to = '/'>
                     <Button 
                         className = { classes.textLink } 
@@ -83,9 +83,9 @@ const LandingNav = ( props ) => {
                     onClick = { session.isUserSignedIn() ? props.signOut : props.signIn }
                     > { session.isUserSignedIn() ? 'Logout' : 'Login' } 
                 </Button>
-            </NavLinks>
-        </Nav>
-    )
-}
+            </NavLinks> */}
+    </Nav>
+  );
+};
 
-export default withStyles( styles )( LandingNav );
+export default withStyles(styles)(LandingNav);
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,28 +1,29 @@
-import { Button } from '@material-ui/core';
-import styled from 'styled-components';
-import React, { /* useState */ } from 'react';
+import { Button } from "@material-ui/core";
+import styled from "styled-components";
+import React /* useState */ from "react";
 
 const TopStyles = styled.div`
-    height: 90vh;
-`
-    const Header = styled.h2`
-        width: 55vw;
-        margin-top: 5%;
-        margin-left: 3%;
-        font-size: 1.2rem;
-        letter-spacing: 2px;
-        color: #393a4d;
-        border-bottom: solid #393a4d 2px;
-    `
+  height: 90vh;
+`;
+const Header = styled.h2`
+  width: 55vw;
+  margin-top: 5%;
+  margin-left: 3%;
+  font-size: 1.2rem;
+  letter-spacing: 2px;
+  color: #393a4d;
+  border-bottom: solid #393a4d 2px;
+`;
 
-    const Photo = styled.img`
-        height: 75vh;
-        opacity: 0.7;
-        margin-top: 6%;
-        opacity: .1;
-    `
+const Photo = styled.img`
+  height: 75vh;
+  opacity: 0.7;
+  margin-top: 6%;
+  opacity: 0.1;
+`;
 
-const Modal = styled.div`{
+const Modal = styled.div`
+   {
     height: 30vh;
     width: 60vw;
     border: none;
@@ -34,9 +35,11 @@ const Modal = styled.div`{
     box-shadow: 1px 1px 1px 1px gray;
     box-shadow: 0px 0px 20px -7px rgba(0, 0, 0, 0.5);
     opacity: 1;
-}`
+  }
+`;
 
-const SubscribeInput = styled.input`{
+const SubscribeInput = styled.input`
+   {
     border: solid black 1px;
     height: 20px;
     width: 20vw;
@@ -44,36 +47,39 @@ const SubscribeInput = styled.input`{
     margin-left: 3%;
     padding-left: 15px;
     font-size: 1.2rem;
-}`
+  }
+`;
 
 const Landing = () => {
-    return (
-        <div>
-            <TopStyles >
-                <Photo src='/images/undraw_posting.svg' alt='calender' />
-                <Modal >
-                    <Header> 
-                        Meet
-                        <span style = {{
-                                fontSize: '20px',
-                                color: '#381b57'
-                        }}> Nuancity 
-                        </span> In 2020.
-                    </Header>
-                    <div>
-                        <SubscribeInput />
-                        <Button
-                            variant = 'contained'
-                            color = 'primary'
-                            size = 'large'
-                        > Stay Updated 
-                        </Button>
-                    </div>
-                </Modal>
-
-            </TopStyles>
-        </div>
-    )
-}
+  return (
+    <div>
+      <TopStyles>
+        <Photo src="/images/undraw_posting.svg" alt="calender" />
+        <Modal>
+          <Header>
+            Meet
+            <span
+              style={{
+                fontSize: "20px",
+                color: "#381b57",
+              }}
+            >
+              {" "}
+              Nuancity
+            </span>{" "}
+            In 2021.
+          </Header>
+          {/* <div>
+            <SubscribeInput />
+            <Button variant="contained" color="primary" size="large">
+              {" "}
+              Stay Updated
+            </Button>
+          </div> */}
+        </Modal>
+      </TopStyles>
+    </div>
+  );
+};
 
 export default Landing;
